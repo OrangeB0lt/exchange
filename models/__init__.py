f9470d5b661b7cc08c3367ff2763944106f84760
OrangeB0lt <667@holbertonschool.com>
2022-06-26 14:44:21 -0400

    initial commit

---
+++ models/__init__.py
@@ -0,0 +1,16 @@
+import os
+from models.base_model import BaseModel
+from models.user import User
+
+"""CNC - dictionary = { Class Name (string) : Class Type }"""
+
+if os.environ.get('HBNB_TYPE_STORAGE') == 'db':
+    from models.engine import db_storage
+    CNC = db_storage.DBStorage.CNC
+    storage = db_storage.DBStorage()
+else:
+    from models.engine import file_storage
+    CNC = file_storage.FileStorage.CNC
+    storage = file_storage.FileStorage()
+
+storage.reload()
